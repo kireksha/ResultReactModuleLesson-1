5a9420490f7c8b5c5b61fba90925acdef57cf523
Kirill Shakin <shakinkirill@mail.ru>
2024-07-19 17:42:44 +0300

    react module lesson #1 part #2

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,31 +1,66 @@
-// Декларативный
 import logo from './logo.svg';
-// Декларативный
 import './App.css';
+import { createElement, React } from 'react';
 
-// Декларативный
+// export const App = () => {
+// 	const currentYear = new Date().getFullYear();
+
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<span>Current Year: {currentYear}</span>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
 export const App = () => {
-	// Декларативный
 	const currentYear = new Date().getFullYear();
-
-	return (
-		/*Императивный*/
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>Current Year: {currentYear}</span>
-			</header>
-		</div>
+	return React.createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		React.createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('span', null, 'Current Year: ', currentYear),
+		),
 	);
 };
